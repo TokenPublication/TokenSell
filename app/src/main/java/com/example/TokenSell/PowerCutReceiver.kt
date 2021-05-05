0d229d79af679d3f312eb904e6da34d7da509891
26067453 <burak.turun@tokeninc.com>
2021-05-05 15:38:37 +0300

    *PGW integration is complete and operational. *Power Run Out Flow is fully implemented. *Previous Sales Activity has just been started to be constructed.

--- app/src/main/java/com/example/TokenSell/PowerCutReceiver.kt
+++ app/src/main/java/com/example/TokenSell/PowerCutReceiver.kt
@@ -13,10 +13,9 @@ class PowerCutReceiver : BroadcastReceiver() {
         if (intent.action != null && intent.action == "token.intent.ACTION_POWERCUT_RESULT") {
 
             val myIntent = Intent()
-            // Process your result, you will get same paymentBody as usual.
             myIntent.putExtra("paymentBody", intent.getStringExtra("paymentBody"))
-            //Do something with myIntent
-            //Burada ben bu intent üstündeki "paymentBody" bilgisini sharedPreferences'a aktarıp
+
+            //Aşağıda bu intent üstündeki "paymentBody" bilgisini sharedPreferences'a aktarıp
             //Başka bir aktivite olan PreviousSalesActivity'de (GEÇMİŞ) son satışın bilgisi olarak çekiyorum.
             val message = myIntent!!.getStringExtra("paymentBody")
             val sharedPref = context!!.getSharedPreferences("sharedPref", AppCompatActivity.MODE_PRIVATE)
